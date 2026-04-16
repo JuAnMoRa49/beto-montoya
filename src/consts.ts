58a410599d72b096eaca174fdc2464cc902e4a12
JuAnMoRa49 <125929793+JuAnMoRa49@users.noreply.github.com>
2026-04-16 14:39:40 -0600

    Footer updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,29 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Beto Montoya";
+export const SITE_DESCRIPTION =
+  "Bienvenido a mi sitio web personal. Aquí encontrarás información sobre mí, mis proyectos y cómo contactarme. Soy un jugador de padel profesional con una pasión por el deporte y la competencia. Explora mi sitio para conocer más sobre mi carrera, mis logros y mis próximos eventos. ¡Gracias por visitarme!";
+
+export const NAVIGATION = [
+  { label: "Inicio", url: "/" },
+  { label: "Sobre mí", url: "/about" },
+  { label: "Blog", url: "/blog" },
+  { label: "Contacto", url: "#contact" },
+];
+
+export const SOCIALS = [
+  {
+    label: "Facebook",
+    url: "https://www.facebook.com/beto.montoya.5",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/beto_montoya_/",
+  },
+
+  {
+    label: "Twitter",
+    url: "https://twitter.com/BetoMontoyaPad",
+  },
+];
